55de9623afd57afcfb0d85859bac00ef107aa65b
jish4593k <143131599+jish4593k@users.noreply.github.com>
2023-11-04 21:14:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,79 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+
+# Sample text data and labels
+texts = ["This is a positive sentence.", "This is a negative sentence.", "Another positive example."]
+labels = [1, 0, 1]
+
+# Tokenize the text data using Keras
+tokenizer = Tokenizer(num_words=1000)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+X = pad_sequences(sequences)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, random_state=42)
+
+# Convert data to PyTorch tensors
+X_train = torch.LongTensor(X_train)
+y_train = torch.FloatTensor(y_train)
+X_test = torch.LongTensor(X_test)
+y_test = torch.FloatTensor(y_test)
+
+# Define a simple neural network using PyTorch
+class SimpleNN(nn.Module):
+    def __init__(self, input_dim):
+        super(SimpleNN, self).__init()
+        self.fc1 = nn.Linear(input_dim, 64)
+        self.fc2 = nn.Linear(64, 1)
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x
+
+# Create a data loader for PyTorch
+class TextDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+    
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+train_dataset = TextDataset(X_train, y_train)
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+
+# Initialize the model and optimizer
+input_dim = X_train.shape[1]
+model = SimpleNN(input_dim)
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+epochs = 10
+for epoch in range(epochs):
+    for batch in train_loader:
+        inputs, labels = batch
+        optimizer.zero_grad()
+        outputs = model(inputs.float())
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+# Evaluate the model on the test set
+model.eval()
+with torch.no_grad():
+    test_outputs = model(X_test.float())
+    test_outputs = (test_outputs > 0.5).float()
+    accuracy = torch.sum(test_outputs == y_test).item() / len(y_test)
+    print("Test Accuracy: {:.2f}%".format(accuracy))
